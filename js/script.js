6795122b2c7d9f4237c423aced7cffd66b3a205d
rivaannn <mrivans2002@gmail.com>
2022-08-26 14:55:07 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 var musik = "";
 
 AOS.init({
-    once: true,
+  once: true,
 });
 
-var audio = document.querySelector(".audio");
+var audio = document.querySelector("audio");
 if (musik) {
-    audio.src = musik;
+  audio.src = musik;
 }
 
 function mulai() {
-    audio.play();
-    document.querySelector(".open").style = "opacity: 0;";
-    document.querySelector(".body").style = "overflow-y: scroll;";
-    setTimeout(function() {
-        document.querySelector(".open").style.display = "none";
-    }, 1000);
+  audio.play();
+  document.querySelector(".open").style = "opacity: 0;";
+  document.querySelector(".body").style = "overflow-y: scroll;";
+  setTimeout(function () {
+    document.querySelector(".open").style.display = "none";
+  }, 1000);
 }
 
 function wa(isi) {
-    window.open(
-        "https://api.whatsapp.com/send?text= thank you for remembering my birthday, i just wanna say,  " +
-        isi
-    );
+  window.open(
+    "https://api.whatsapp.com/send?text= thank you for remembering my birthday, i just wanna say,  " +
+      isi
+  );
 }
 async function tanya() {
-    var { value: kado } = await swal.fire({
-        title: "is there a message you want to convey to me?",
-        input: "text",
-        showCancelButton: false,
-    });
-    if (kado) {
-        await swal.fire("Send the answer to my whatsapp okay.");
-        wa(kado);
-    } else {
-        await swal.fire("don't leave the answer blank😡 ");
-        tanya();
-    }
+  var { value: kado } = await swal.fire({
+    title: "is there a message you want to convey to me?",
+    input: "text",
+    showCancelButton: false,
+  });
+  if (kado) {
+    await swal.fire("Send the answer to my whatsapp okay.");
+    wa(kado);
+  } else {
+    await swal.fire("don't leave the answer blank😡 ");
+    tanya();
+  }
 }
