d4581f97491530118a7b75978006bc013e11d146
rivaannn <mrivans2002@gmail.com>
2023-08-26 22:05:39 +0700

    new update

--- js/script.js
+++ js/script.js
@@ -48,11 +48,3 @@ function toggleAudio() {
     audio.currentTime = 0;
   }
 }
-
-function tampilkanKonfeti() {
-  confetti({
-    particleCount: 150,
-    spread: 70,
-    origin: { y: 0.6 },
-  });
-}
